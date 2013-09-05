c15131001f5565d924e3af25d1468e0ecc74ddd0
Ned KY <nkaidayip@gmail.com>
2013-09-04 21:33:49 -0500

    updates gemfile and database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RestMan::Application.config.secret_token = 'cfd852ff8ebbf0ec388a52725c07c39fd31e476905b87052c68f8a2a9651bc506089714998f2fec8715d66342719caee5bc17cd12be3a3b05fa845bb9bc2c2d2'
