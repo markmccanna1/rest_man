7a8d026f14fc158049c6b4f99c66260370e4526b
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-05 11:07:12 -0500

    example square is draggable and clickable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+$('document').ready(function() {
+
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 RestMan::Application.routes.draw do
   get '/customer/find/restaurant_profiles', to: "restaurant_profiles#find"
-  get '/restaurant_profiles/find', to: "restaurant_profiles#search" 
+  get '/restaurant_profiles/find', to: "restaurant_profiles#search"
   get 'restaurant_profiles/new', to: "restaurant_profiles#new"
   post 'restaurant_profiles/create', to: "restaurant_profiles#create"
 
@@ -74,7 +74,6 @@ RestMan::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'customer_profiles#new'
-
 
   # See how all your routes lay out with "rake routes"
 
