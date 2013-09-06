b12182aa3c085211e742a49e596fd80d6e604358
Ned KY <nkaidayip@gmail.com>
2013-09-05 20:22:38 -0500

    added a creation form for a new menu item

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
-
+  
+  attr_accessible :email, :password, :password_confirmation, :phone_number
   validates_presence_of (:email)
   validates_uniqueness_of (:email)
   
