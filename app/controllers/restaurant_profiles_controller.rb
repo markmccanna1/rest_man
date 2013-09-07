6b665fdb7ca94fac0c0f1bf7c5abf7afa14c8798
Apprentice <apprentice@dbc-chicago-001.local>
2013-09-07 15:38:28 -0500

    updated search to use like and logout to reset session
    moved search to different controller

--- app/controllers/restaurant_profiles_controller.rb
+++ app/controllers/restaurant_profiles_controller.rb
@@ -23,13 +23,4 @@ class RestaurantProfilesController < ApplicationController
     @menu = @restaurant.menus.first
     @order = Order.new
   end
-
-  def find
-    @restaurants = RestaurantProfile.all
-  end
-
-  def search
-    restaurant = RestaurantProfile.find_by_restaurant_name(params[:name])
-    redirect_to restaurant_profile_url(restaurant) 
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 RestMan::Application.routes.draw do
-  get '/customer/find/restaurant_profiles', to: "restaurant_profiles#find"
-  get '/restaurant_profiles/find', to: "restaurant_profiles#search" 
+  get '/customer/find/restaurant_profiles', to: "customer_profiles#find"  
+  get '/customer_profiles/find', to: "customer_profiles#search" 
   get 'restaurant_profiles/new', to: "restaurant_profiles#new"
   post 'restaurant_profiles/create', to: "restaurant_profiles#create"
 
