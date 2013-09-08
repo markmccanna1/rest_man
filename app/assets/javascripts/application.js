6d8d844112fde3e4db4c691b0ecd46d861ac4d46
Apprentice <apprentice@dbc-chicago-001.local>
2013-09-07 21:09:36 -0500

    when you click a table you create a form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -54,7 +54,7 @@ var AddTableButton = {
 
     //set the svg element id
     nested.attr({id: 'createTable'})
-    console.log(nested)
+    // console.log(nested)
 
     this.width = 120
     this.height = 120
@@ -70,13 +70,12 @@ var AddTableButton = {
     this.drawing.center('5%', '15%')
     this.drawing.click(this.addClickEvent)
     this.tableCounter = 1
-    this.tables = new Array()
   },
 
   addClickEvent: function(){
     var table = new Table(AddTableButton.tableCounter)
     AddTableButton.tableCounter += 1
-    AddTableButton.tables.push(table)
+    FloorPlan.tables.push(table)
   },
 
   addForeignObject: function(){
@@ -101,19 +100,50 @@ var AddTableButton = {
 >>>>>>> updated namespacing
   },
 
-    getTableById: function(id){
-    table = FloorPlan.tables.filter(function(element) {return element.table.attr('id') === id
+  getTableById: function(id){
+    var table = FloorPlan.tables.filter(function(element) {return element.drawing.attr('id') === id
     })
     return table[0]
   }
 
 }
 
+function Form(tableId) {
+  this.tableId = tableId
+  this.addDomElements()
+}
+
+Form.prototype = {
+  addDomElements: function(){
+    // console.log(this.tableId)
+
+    var table = AddTableButton.getTableById(this.tableId)
+    console.log(table)
+    var foreignObject = document.createElementNS( 'http://www.w3.org/2000/svg','foreignObject' );
+    foreignObject.setAttribute('x', 0)
+    foreignObject.setAttribute('y', 0)
+    foreignObject.setAttribute('width', table.width)
+    foreignObject.setAttribute('height', table.height)
+    var body = document.createElement('body')
+    $(body).append(this.form  )
+    $(foreignObject).append(body)
+    document.getElementById('svg_' + this.tableId ).appendChild( foreignObject )
+  },
+
+  form: function(tableId){
+    var chairForm = '<form><input type="text"><input type="submit"></form>'
+    return chairForm
+  }
+
+
+}
+
 function Table(id) {
   var nested = FloorPlan.drawing.nested()
-  
   nested.attr({id: 'svg_table' + id})
-  this.drawing = nested.circle(100,100).attr({fill: 'white', class: 'table', id: 'table' + id})
+  this.width = 100
+  this.height = 100
+  this.drawing = nested.circle(this.width,this.height).attr({fill: 'white', class: 'table', id: 'table' + id})
   this.drawing.stroke({color: 'black', width: 2})
   this.drawing.draggable()
   //i dont like these positions
@@ -126,23 +156,42 @@ function Table(id) {
 Table.prototype = {
   addClickEvent: function(){
 
+
+
+
     //in this click event, popup a form for this particular table...
-    console.log(this)
-    console.log(this.attr('id'))
+    // console.log(this)
+    // console.log(this.attr('id'))
+
+    var tableId = this.attr('id')
+
+    var form = new Form(tableId)
     //in this click event, the this is the svg element....
     //how do you access its actual dom element...
     //you need to be able to access the js elements chairs array
     //create an event where you can edit the number of chairs per table
     //do i need to put the counter on another object?
+    // addForeignObject
     
   },
 
   returnChairs: function(){
     return this.chairs
-  }
+  },
   //when you add chairs you want to increment the chair counter on the object
   //what happens if they remove chairs?
   //you have to make the 'chairs' variable dependend on their form submission... kinky
+  addForeignObject: function(){
+    var foreignObject = document.createElementNS( 'http://www.w3.org/2000/svg','foreignObject' );
+    foreignObject.setAttribute('x', 0)
+    foreignObject.setAttribute('y', 0)
+    foreignObject.setAttribute('width', this.width)
+    foreignObject.setAttribute('height', this.height)
+    var body = document.createElement('body')
+    $(body).append('<div> hi </div>')
+    $(foreignObject).append(body)
+    document.getElementById( 'createTable' ).appendChild( foreignObject );
+  }
 }
 
 
