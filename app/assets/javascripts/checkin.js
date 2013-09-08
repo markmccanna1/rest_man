df37466af12e88bf2b6ee672cc664ae1276d210a
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-08 15:25:56 -0500

    updated floorplan routing

--- app/assets/javascripts/checkin.js
+++ app/assets/javascripts/checkin.js
@@ -2,17 +2,18 @@
 $(document).ready(function(){
   $('.seat').on('click', function(e){
     var seat_id = $(this).data('seat-id');
-    var token = $('meta[name="csrf-token"]').attr('content')
+    var token = $('meta[name="csrf-token"]').attr('content');
+
     if ($(this).hasClass('taken')){
       $.post('/check_out', {seat_id: seat_id, token: token}, function(){
-       $(this).removeClass('taken');
+        $(this).removeClass('taken');
       });
     } else {
       $.post('/check_in', {seat_id: seat_id, token: token});
         $(this).addClass('taken');
     }
-  })
-  var url_id = window.location.pathname.split('/').reverse()[1]
+  });
+  var url_id = window.location.pathname.split('/').reverse()[1];
 
   if ($('body.floor_plan').length) {
     $.getJSON("/restaurant_profiles/" + url_id + "/floor_plan", function(data){
