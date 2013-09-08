90b01f96f7668fa699e9c4fcdb051e813a88c73c
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-08 15:25:56 -0500

    updated floorplan routing

--- app/assets/javascripts/restaurant/restaurant.js
+++ app/assets/javascripts/restaurant/restaurant.js
@@ -10,24 +10,24 @@ var Carts = {
         $.each(v, function(i, l){
           $('.pending-carts').append('<li><a href="carts/' + l.id + '">' + l.id + '</a></li>')
         })
-      })    
+      })
     })
   }
 }
 
 
-var Stream = {
+// var Stream = {
 
-  init: function(){
-    window.setInterval(this.update, 5000)
-  },
+//   init: function(){
+//     window.setInterval(this.update, 5000)
+//   },
 
-  update: function() {
-    $.get('/new_items', function(data){
-        console.log(data)
-    })
-  }
-}
+//   update: function() {
+//     $.get('/new_items', function(data){
+//         console.log(data)
+//     })
+//   }
+// }
 
 var newMenu = {
   init: function(){
@@ -42,9 +42,9 @@ var newMenu = {
 }
 
 $('document').ready(function() {
-  if ($('body.restaurant_profiles').length) {
-    Stream.init()
-  }
+  // if ($('body.restaurant_profiles').length) {
+  //   Stream.init()
+  // }
   if ($('.active-carts').length){
     Carts.init()
   }
