f628a103ce43108bf2d8614b17a63dfc64c9ae82
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-10 19:00:24 -0500

    added successful redirect after checkin

--- app/controllers/seats_controller.rb
+++ app/controllers/seats_controller.rb
@@ -3,17 +3,11 @@ class SeatsController < ApplicationController
   before_filter :authorize_restaurant, :except => [:check_in, :index]
 
   def check_in
-    puts params
     floor_plan = FloorPlan.find(params[:url_id])
-    p floor_plan
     seat = floor_plan.seats.find_by_html_id(params[:seat_id])
     seat.update_attributes(customer_profile_id: session[:customer_profile_id])
-    p session[:customer_profile_id]
     restaurant = seat.table.floor_plan.restaurant_profile
-    respond_to do |format|
-      format.html { redirect_to restaurant_profile_url(restaurant) }
-      format.js
-    end
+    render :js => "window.location.href = '#{restaurant_profile_url(restaurant)}'"
   end
 
 end
